71c489afc294aa1e8d7c41fa982f2968ad27515f
Light3039 <VerodiciaTheNatureGoddess@gmail.com>
2021-04-28 12:06:32 +0430

    Demo changes
    - Reorganized some demo stuff

---
+++ Assets/Scripts/Demo/MusicPlayer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioEmitterST emitter;
+
+    [SerializeField] private Slider timelineSlider, pitchSlider;
+
+    private void Start()
+    {
+        timelineSlider.minValue = 0f;
+        timelineSlider.maxValue = emitter["music"].GetLength();
+        timelineSlider.wholeNumbers = true;
+
+        pitchSlider.value = emitter["music"].GetParameter("Sadness");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+            SceneManager.LoadNextScene();
+
+        timelineSlider.value = emitter["music"].GetTimelinePosition();
+    }
+
+    public void OnButtons()
+    {
+        emitter["music"].Pause();
+    }
+
+    public void OnTimelineSlider(float value)
+    {
+        if ((int)value != emitter["music"].GetTimelinePosition())
+            emitter["music"].SetTimelinePosition((int)value);
+    }
+
+    public void OnPitchSlider(float value)
+    {
+        emitter["music"].SetParameter("Sadness", value);
+    }
+
+}
---
+++ Assets/Scripts/Demo/SFXPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SFXPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private AudioEmitterFX emitter;
+
+    [SerializeField]
+    private Slider pitchSlider, globalSlider;
+
+    private void Start()
+    {
+        pitchSlider.value = emitter["sfx2"].GetParameter("Pitch");
+        globalSlider.value = emitter["sfx2"].GetParameter("Global");
+    }
+
+    public void OnButtons(string btn)
+    {
+        // 'btn' can be 1, 2 or 3
+        emitter["sfx" + btn].Start();
+    }
+
+    public void OnPitchSlider(float value)
+    {
+        emitter["sfx2"].SetParameter("Pitch", value);
+    }
+
+    public void OnGlobalSlider(float value)
+    {
+        emitter["sfx2"].SetGlobalParameter("Global", value);
+    }
+
+}
