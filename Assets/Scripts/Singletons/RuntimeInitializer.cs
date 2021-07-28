01c370b0ba65c996de7da4749d57910f3b70b78b
Light <VerodiciaTheNatureGoddess@gmail.com>
2021-07-28 10:03:19 +0430

    RuntimeInitializer
    - Replaced SingletonManager
    - Added RuntimeInitializer

---
+++ Assets/Scripts/Singletons/RuntimeInitializer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+class RuntimeInitializer
+{
+    // Order is from the top-most function to the bottom-most function
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    private static void OnSubsystemRegistration()
+    {
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+    private static void OnAfterAssembliesLoaded()
+    {
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+    private static void OnBeforeSplashScreen()
+    {
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void OnBeforeSceneLoad()
+    {
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    private static void OnAfterSceneLoad()
+    {
+    }
+
+}
